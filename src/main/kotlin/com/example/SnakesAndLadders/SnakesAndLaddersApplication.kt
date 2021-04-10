18f64a56830fdeaa8f4cf5a46cf6c8694dee8e3a
Vladyslav Kharchenko <vladyslav.kharchenko.jd@gmail.com>
2021-04-10 21:23:03 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/SnakesAndLadders/SnakesAndLaddersApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SnakesAndLadders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnakesAndLaddersApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnakesAndLaddersApplication>(*args)
+}
